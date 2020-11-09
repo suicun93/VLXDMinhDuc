701a6218424af1d6aff131987ad82b2f8da540b1
ducht <duchoang191@gmail.com>
2020-11-09 10:33:17 +0700

    Hoàn thành trang báo cáo

---
+++ js/messages.vn-vi.js
@@ -0,0 +1,53 @@
+gj.dialog.messages['vn-vi'] = {
+    Close: 'Đóng',
+    DefaultTitle: 'ダイアログ'
+};
+gj.grid.messages['vn-vi'] = {
+    First: '最初',
+    Previous: '前',
+    Next: '次',
+    Last: '最後',
+    Page: 'ページ',
+    FirstPageTooltip: '最初のページ',
+    PreviousPageTooltip: '前のページ',
+    NextPageTooltip: '次のページ',
+    LastPageTooltip: '最後のページ',
+    Refresh: 'リフレッシュ',
+    Of: 'của',
+    DisplayingRecords: '結果',
+    RowsPerPage: 'ページあたり行数:',
+    Edit: 'Sửa',
+    Delete: 'Xóa',
+    Update: 'Cập nhật',
+    Cancel: 'Hủy bỏ',
+    NoRecordsFound: 'レコードが見つかりません',
+    Loading: 'Đang tải...'
+};
+gj.editor.messages['vn-vi'] = {
+    bold: '太字',
+    italic: '斜体',
+    strikethrough: '打消し線',
+    underline: '下線',
+    listBulleted: '箇条書き',
+    listNumbered: '番号付き箇条書き',
+    indentDecrease: 'インデントを減らす',
+    indentIncrease: 'インデントを増やす',
+    alignLeft: '左揃え',
+    alignCenter: '中央揃え',
+    alignRight: '右揃え',
+    alignJustify: '両端揃え',
+    undo: '元に戻す',
+    redo: 'やり直し'
+};
+gj.core.messages['vn-vi'] = {
+    monthNames: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+    monthShortNames: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+    weekDaysMin: ['CN', 'Hai', 'Ba', 'Tư', 'Năm', ' Sáu', 'Bảy'],
+    weekDaysShort: ['CN', 'Hai', 'Ba', 'Tư', 'Năm', ' Sáu', 'Bảy'],
+    weekDays: ['CN', 'Hai', 'Ba', 'Tư', 'Năm', ' Sáu', 'Bảy'],
+    am: 'Sáng',
+    pm: 'Chiều',
+    ok: 'OK',
+    cancel: 'Hủy bỏ',
+    titleFormat: 'Tháng mmmm Năm yyyy'
+};
